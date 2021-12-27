8ab12e43716520ac2cd6055f7f747d7a22f5ec51
Stridsvagn69420 <Stridsvagn69420@users.noreply.github.com>
2021-12-27 02:19:27 +0100

    Reading directory is done. Only renaming left.

--- rename.go
+++ rename.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"os"
 )
 
@@ -10,7 +11,7 @@ func main() {
 	// presets
 	path, err := os.Getwd()
 	if err != nil {
-		path = "."
+		path = "./"
 	}
 	// set cli flags
 	flag.StringVar(&path, "D", path, "Directory of files to rename")
@@ -26,16 +27,42 @@ func main() {
 		errorFlags = true
 	}
 	if *substring == "" {
-		os.Stderr.WriteString("\033[1;31mSubstring not set but required!\033[0m")
+		os.Stderr.WriteString("\033[1;31mSubstring not set but required!\033[0m\n")
 		errorFlags = true
 	}
 	if errorFlags {
+		fmt.Print("To get help, run: \033[1;32mrename -h\033[0m")
 		os.Exit(1)
 	}
 
-	fmt.Println(path)
-	fmt.Println(*extension)
-	fmt.Println(*substring)
-	fmt.Println(*replacement)
+	// read files
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		os.Stderr.WriteString(err.Error() + "\n")
+	}
+	files = filterFiles(files, *extension)
 
+	// rename files
+	for _, file := range files {
+		fmt.Print(file.Name())
+	}
+
+	// exit normally
+	os.Exit(0)
+}
+
+func filterFiles(files []os.FileInfo, extension string) []os.FileInfo {
+	filteredFiles := make([]os.FileInfo, 0)
+	for _, file := range files {
+		if !file.IsDir() {
+			if extension != "*" {
+				if file.Name()[len(file.Name())-len(extension):] == extension {
+					filteredFiles = append(filteredFiles, file)
+				}
+			} else {
+				filteredFiles = append(filteredFiles, file)
+			}
+		}
+	}
+	return filteredFiles
 }
